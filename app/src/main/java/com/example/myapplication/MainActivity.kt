30950dade4599dea3bc235befd22008523077349
hakimovhasanboy <2023158046@tukorea.ac.kr>
2024-12-22 14:08:39 +0500

    first commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,26 +7,67 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    var cmToM = true
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        val numberTextView = findViewById<TextView>(R.id.numberTextView)
-        val resetButton = findViewById<Button>(R.id.resetButton)
-        val plusButton = findViewById<Button>(R.id.plusButton)
+        val outputTextView = binding.outputTextView
+        val outputUnitTextView = binding.outputUnitTextView
+        val inputEditText = binding.inputEditText
+        val inputUnitTextView = binding.inputUnitTextView
+        val swapImageButton = binding.swapImageButton
 
-        var number: Int = 0
+        var inputNumber: Int = 0
 
-        resetButton.setOnClickListener {
-            number = 0
-            numberTextView.text = number.toString()
-        }
+        inputEditText.addTextChangedListener {text ->
+            inputNumber = if (text.isNullOrEmpty()) {
+                0
+            } else {
+                text.toString().toInt()
+            }
 
-        plusButton.setOnClickListener {
-            number += 1
-            numberTextView.text = number.toString()
+            if (cmToM) {
+                outputTextView.text = inputNumber.times(0.01).toString()
+            } else {
+                outputTextView.text = inputNumber.times(100).toString()
+            }
+
+            swapImageButton.setOnClickListener {
+                cmToM = cmToM.not()
+                if (cmToM) {
+                    inputUnitTextView.text = "cm"
+                    outputUnitTextView.text = "m"
+                    outputTextView.text = inputNumber.times(0.01).toString()
+                } else {
+                    inputUnitTextView.text = "m"
+                    outputUnitTextView.text = "cm"
+                    outputTextView.text = inputNumber.times(100).toString()
+                }
+            }
         }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putBoolean("cmToM", cmToM)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        cmToM = savedInstanceState.getBoolean("cmToM")
+        Log.d("cmToM", cmToM.toString())
+        binding.inputUnitTextView.text = if (cmToM) "cm" else "m"
+        binding.outputUnitTextView.text = if (cmToM) "m" else "cm"
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
+
 }
